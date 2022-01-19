d2591c2aad4094c6330e5e7e567bf0f0d443bfff
Max Li <maxl@usc.edu>
2022-01-18 23:52:55 -0800

    Update create_playlist.py

--- create_playlist.py
+++ create_playlist.py
@@ -11,21 +11,17 @@ class CreatePlaylist:
         self.youtube_client = self.getYoutubeClient()
         self.all_song_info = {}
     def getYoutubeClient(self):
-                # Disable OAuthlib's HTTPS verification when running locally.
-        # *DO NOT* leave this option enabled in production.
         os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
         api_service_name = "youtube"
         api_version = "v3"
         client_secrets_file = "client_secret.json"
 
-        # Get credentials and create an API client
         scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
         flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
             client_secrets_file, scopes)
         credentials = flow.run_console()
 
-        # from the Youtube DATA API
         youtube_client = googleapiclient.discovery.build(
             api_service_name, api_version, credentials=credentials)
 
